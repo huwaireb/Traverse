df2205bc277b5d505ee1ca9a8257b9c339d4a0ef
Rashid J. Almheiri <r.muhairi@pm.me>
2025-04-10 10:44:34 +0400

    feat: use flatbuffers

--- src/ui/Root.swift
+++ src/ui/Root.swift
@@ -2,30 +2,7 @@ import RealityKit
 import SwiftUI
 
 struct Root: View {
-    var body: some View = RealityView { scene in
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(
-            color: .systemBlue,
-            roughness: 0.15,
-            isMetallic: true
-        )
-
-        let component = ModelComponent(mesh: mesh, materials: [material])
-        let model = Entity(components: [component])
-
-        model.position = [0, 0.05, 0]
-        let anchor = AnchorEntity(
-            .plane(
-                .horizontal,
-                classification: .any,
-                minimumBounds: SIMD2<Float>(0.2, 0.22)
-            )
-        )
-
-        anchor.addChild(model)
-
-        scene.add(anchor)
-        scene.camera = .spatialTracking
+    var body: some View = RealityView { _ in
     }.edgesIgnoringSafeArea(.all)
 }
 
